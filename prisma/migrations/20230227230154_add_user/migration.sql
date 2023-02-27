0a43c7f50b2e631cf7efc48196286d01eff0d556
mioe <mioe.boss@gmail.com>
2023-02-28 02:06:18 +0300

    add User

---
+++ prisma/migrations/20230227230154_add_user/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "username" TEXT NOT NULL,
+    "paymentMethodId" TEXT,
+    "token" TEXT,
+    "userInfo" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
